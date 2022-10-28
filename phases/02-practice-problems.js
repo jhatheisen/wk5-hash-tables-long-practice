ba9b9fe2d7e0113d8c384dc1ba1994c982f27518
Jhass Theisen <jhasstheisen1@gmail.com>
2022-10-27 17:00:20 -0700

    created comments and logs 'mocha -g wordPattern'

--- phases/02-practice-problems.js
+++ phases/02-practice-problems.js
@@ -84,29 +84,36 @@ function twoSum(nums, target) {
 
 
 function wordPattern(pattern, strings) {
-  let patternSet = new Set(pattern.split(""));
-  let patternArray = Array.from(patternSet)
+  // make pattern an arr
+  pattern = pattern.split('');
+
+  // get only uniques of pattern and string
+  let patternArray = [... new Set(pattern)]
   console.log(patternArray)
-  let stringSet = new Set(strings)
-  let stringArray = Array.from(stringSet);
+  let stringArray = [... new Set(strings)];
   console.log(stringArray);
-  let stringObj = {};
+
+  // if too many uniques, can't be pattern, return false
   if (stringArray.length !== patternArray.length) return false;
+
+  let stringObj = {};
+  // iterate through arrays
   for (let i = 0; i < patternArray.length; i++) {
+    // create key value pair for each el of arr
     stringObj[stringArray[i]] = patternArray[i];
   }
   console.log(stringObj);
-  // console.log(patternSet)
-  // console.log(stringSet)
+
+  // set each string to equal it's character in pattern
   for (let i = 0; i < strings.length; i++) {
     if (strings[i] in stringObj) {
       strings[i] = stringObj[strings[i]];
     }
   }
-  let a = pattern.split('');
-  let b = strings;
-  for (var i = 0; i < a.length; ++i) {
-    if (a[i] !== b[i]) return false;
+
+  // check if pattern === strings , return result
+  for (var i = 0; i < pattern.length; ++i) {
+    if (pattern[i] !== strings[i]) return false;
   }
   return true;
 }
